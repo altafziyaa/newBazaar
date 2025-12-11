89948f9428441938263571339fbf2dbe72eba5ee
MD.ALTAF <117305778+altafziyaa@users.noreply.github.com>
2025-12-11 23:07:34 +0530

    Delete Music/OneDrive/Desktop/New folder directory

--- Music/OneDrive/Desktop/New folder/server/src/domain/OrderStatus.js
+++
@@ -1,8 +0,0 @@
-const OrderStatus = Object.freeze({
-  PENDING: "pending",
-  PLACED: "placed",
-  SHIPPED: "shipped",
-  DELIVERED: "delivered",
-  CANCELLED: "cancelled",
-});
-export default OrderStatus;
--- Music/OneDrive/Desktop/New folder/server/src/routes/AdminRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import sellerController from '../controller/SellerController.js'; 
-
-router.patch('/seller/:id/status/:status', sellerController.updateSellerAccStatus);
-   
-
-export default router;
